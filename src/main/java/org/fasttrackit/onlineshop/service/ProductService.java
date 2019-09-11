b1ef88142550c7618154187c0eb235f3a0fec1c4
Raul <ratiugabrielraul@gmail.com>
2019-09-11 21:12:47 +0300

    added product service and create product tests

---
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -0,0 +1,42 @@
+package org.fasttrackit.onlineshop.service;
+
+import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.persistance.ProductRepository;
+import org.fasttrackit.onlineshop.transfer.product.SaveProductRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class ProductService {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
+    //IoC -Inversion of Control
+    private  final ProductRepository productRepository;
+
+    //dependency injection
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(SaveProductRequest request) {
+        LOGGER.info("Creating product: {}", request);
+
+        Product product = new Product();
+        product.setName(request.getName());
+        product.setPrice(request.getPrice());
+        product.setDescription(request.getDescription());
+        product.setQuantity(request.getQuantity());
+        product.setImagePath(request.getImagePath());
+
+        return productRepository.save(product);
+
+
+
+
+
+    }
+}
---
+++ src/test/java/org/fasttrackit/onlineshop/ProductServiceIntegrationTests.java
@@ -0,0 +1,55 @@
+package org.fasttrackit.onlineshop;
+
+
+import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.service.ProductService;
+import org.fasttrackit.onlineshop.transfer.product.SaveProductRequest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.TransactionSystemException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductServiceIntegrationTests {
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    public void testCreateProduct_whenValidRequest_theReturnCreatedProduct() {
+        SaveProductRequest request = new SaveProductRequest();
+        request.setName("Computer");
+        request.setDescription("Some description");
+        request.setPrice(2000);
+        request.setQuantity(100);
+
+        Product product = productService.createProduct(request);
+
+        assertThat(product, notNullValue());
+        assertThat(product.getId(),notNullValue());
+        assertThat(product.getId(),greaterThan((0L)));
+        assertThat(product.getName(), is(request.getName()));
+        assertThat(product.getDescription(), is(request.getDescription()));
+        assertThat(product.getQuantity(), is(request.getQuantity()));
+        assertThat(product.getPrice(), is(request.getPrice()));
+
+ }
+
+    @Test(expected = TransactionSystemException.class)
+    public void testCreateProduct_whenInvalidRequest_thenThrowException() {
+        SaveProductRequest request = new SaveProductRequest();
+        //we're not setting any values on the request
+        // because we want to send an invalid request
+
+        productService.createProduct(request);
+
+    }
+
+}
