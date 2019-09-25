c6fb6d7800200a5489d50dae4b9479c5d1c69bb4
Raul <ratiugabrielraul@gmail.com>
2019-09-25 19:23:52 +0300

    added all endpoints in productController

---
+++ src/main/java/org/fasttrackit/onlineshop/web/ProductController.java
@@ -0,0 +1,59 @@
+package org.fasttrackit.onlineshop.web;
+
+import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.service.ProductService;
+import org.fasttrackit.onlineshop.transfer.product.GetProductRequest;
+import org.fasttrackit.onlineshop.transfer.product.SaveProductRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody @Valid SaveProductRequest request) {
+
+        Product product = productService.createProduct(request);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") long id) {
+        Product product = productService.getProduct(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Product>> getProduct(GetProductRequest request, Pageable pageable) {
+        Page<Product> products = productService.getProducts(request, pageable);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") long id, @RequestBody SaveProductRequest request) {
+        Product product = productService.updateProduct(id, request);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable("id") long id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
