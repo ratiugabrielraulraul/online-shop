92c500bc74717a292c9ace3137fd8e465287962d
Raul <ratiugabrielraul@gmail.com>
2019-10-09 19:49:41 +0300

    added cart controller and handled CrossOrigin

--- src/main/java/org/fasttrackit/onlineshop/web/ProductController.java
+++ src/main/java/org/fasttrackit/onlineshop/web/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/products")
 public class ProductController {
