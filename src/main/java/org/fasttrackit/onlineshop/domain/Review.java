baf384927828b145356622709f072d2a50dbf81f
Raul <ratiugabrielraul@gmail.com>
2019-10-09 19:48:50 +0300

    studied ManyToOne relationship

---
+++ src/main/java/org/fasttrackit/onlineshop/domain/Review.java
@@ -0,0 +1,41 @@
+package org.fasttrackit.onlineshop.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+}
---
+++ src/main/java/org/fasttrackit/onlineshop/persistance/ReviewRepository.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.onlineshop.persistance;
+
+import org.fasttrackit.onlineshop.domain.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    //find by nested properties
+    Page<Review> findByProductId(Long productId, Pageable pageable);
+
+}
